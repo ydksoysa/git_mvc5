c6a09f9ab55e1a460730509ca862c2daf392b3fe
ydksoysa <135942072+ydksoysa@users.noreply.github.com>
2025-07-16 16:08:23 +0530

    complete ItemController class

--- src/edu/ijse/mvc/controller/ItemController.java
+++ src/edu/ijse/mvc/controller/ItemController.java
@@ -6,6 +6,7 @@ package edu.ijse.mvc.controller;
 
 import edu.ijse.mvc.dto.ItemDto;
 import edu.ijse.mvc.model.ItemModel;
+import java.util.ArrayList;
 
 
 /**
@@ -18,6 +19,22 @@ public class ItemController {
      public String saveItem(ItemDto itemDto) throws Exception{
        
          return itemModel.saveItem(itemDto);
+    }
+     
+        public String updateItem(ItemDto itemDto) throws Exception{
+        return itemModel.updateItem(itemDto);
+    }
+    
+    public String deleteItem(String itemId) throws Exception{
+        return itemModel.deleteItem(itemId);
+    }
+    
+    public ItemDto searchItem(String itemId) throws Exception{
+        return itemModel.searchItem(itemId);
+    }
+    
+    public ArrayList<ItemDto> getAllItem() throws Exception{
+        return itemModel.getAllItem();
     }
     
 }
